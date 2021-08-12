4c800c7d7978c32c2b9c3a00307a4ffebe417863
Cloves José <clovesjsilva8@gmail.com>
2021-08-12 10:52:09 -0300

    modificação

--- controle_css/lib/main.dart
+++ controle_css/lib/main.dart
@@ -1,10 +1,1 @@
-import 'package:controle_css/screens/Home.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-      theme: ThemeData(
-        hintColor: Colors.blueAccent,
-        primaryColor: Colors.white,
-      ),
-    ));
